96329038b5faf136adc3a609e6bb6ac465a890d3
Mariah Rivera <rivmariah06@gmail.com>
2020-12-17 18:53:42 -0500

    adding some style and location weather card

--- weather-api.js
+++ weather-api.js
@@ -1,62 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
+// This is our API key
+var APIKey = "166a433c57516f51dfab1f7edaed8413";
 
-<head>
-  <meta charset="utf-8">
-  <title>Bujumbura Data</title>
-</head>
+// Here we are building the URL we need to query the database
+var queryURL =
+  "https://api.openweathermap.org/data/2.5/weather?" +
+  "q=Bujumbura,Burundi&appid=" +
+  APIKey;
 
-<body>
-  <!-- Retrieved data will be dumped here -->
-  <div class="city"></div>
-  <div class="wind"></div>
-  <div class="humidity"></div>
-  <div class="temp"></div>
+// Here we run our AJAX call to the OpenWeatherMap API
+$.ajax({
+  url: queryURL,
+  method: "GET",
+})
+  // We store all of the retrieved data inside of an object called "response"
+  .then(function (response) {
+    // Log the queryURL
+    console.log(queryURL);
 
-  <!-- Add extra div for the bonus Fahrenheit temp -->
-  <div class="tempF"></div>
+    // Log the resulting object
+    console.log(response);
 
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script type="text/javascript">
-    // This is our API key
-    var APIKey = "166a433c57516f51dfab1f7edaed8413";
+    // Transfer content to HTML
+    $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+    $(".wind").text("Wind Speed: " + response.wind.speed);
+    $(".humidity").text("Humidity: " + response.main.humidity);
 
-    // Here we are building the URL we need to query the database
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-      "q=Bujumbura,Burundi&appid=" + APIKey;
+    // Convert the temp to fahrenheit
+    var tempF = (response.main.temp - 273.15) * 1.8 + 32;
 
-    // Here we run our AJAX call to the OpenWeatherMap API
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-      // We store all of the retrieved data inside of an object called "response"
-      .then(function(response) {
+    // add temp content to html
+    $(".temp").text("Temperature (K) " + response.main.temp);
+    $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
 
-        // Log the queryURL
-        console.log(queryURL);
-
-        // Log the resulting object
-        console.log(response);
-
-        // Transfer content to HTML
-        $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-        $(".wind").text("Wind Speed: " + response.wind.speed);
-        $(".humidity").text("Humidity: " + response.main.humidity);
-        
-        // Convert the temp to fahrenheit
-        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-
-        // add temp content to html
-        $(".temp").text("Temperature (K) " + response.main.temp);
-        $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
-
-        // Log the data in the console as well
-        console.log("Wind Speed: " + response.wind.speed);
-        console.log("Humidity: " + response.main.humidity);
-        console.log("Temperature (F): " + tempF);
-      });
-  </script>
-</body>
-
-</html>
+    // Log the data in the console as well
+    console.log("Wind Speed: " + response.wind.speed);
+    console.log("Humidity: " + response.main.humidity);
+    console.log("Temperature (F): " + tempF);
+  });
